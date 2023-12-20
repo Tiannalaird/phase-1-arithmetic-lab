c8a49a3a4981a4ffbc103cf291a89061e3c1e745
TiannaLaird <tiannalairdx123@gmail.com>
2023-12-19 21:48:58 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+let multiply = num1 * num2;
+
+console.log(multiply); // Output: 62
+var random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+console.log(random);
+var num3 = 25;
+var num4 = 7;
+
+// Calculate remainder
+var mod = num3 % num4;
+
+console.log(mod);
+const numbers = [10, 5, 20, 15];
+const max = Math.max(...numbers);
+
+console.log(max); // Output: 20
